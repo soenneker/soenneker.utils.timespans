d25b96d998b1810619649630f1ed13f3da1ffb4c
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:55 -0500

    TUnit conversion

--- test/Soenneker.Utils.TimeSpans.Tests/TimeSpansUtilTests.cs
+++ test/Soenneker.Utils.TimeSpans.Tests/TimeSpansUtilTests.cs
@@ -1,18 +1,17 @@
-﻿using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.Utils.TimeSpans.Tests;
 
-[Collection("Collection")]
-public class TimeSpansUtilTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public class TimeSpansUtilTests : HostedUnitTest
 {
 
-    public TimeSpansUtilTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public TimeSpansUtilTests(Host host) : base(host)
     {
 
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
