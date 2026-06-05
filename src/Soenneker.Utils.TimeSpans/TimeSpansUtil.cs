029ae0a5a9ec7124c4880b7e5ba833fceadacc9e
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:37:49 -0500

    xml commenting

--- src/Soenneker.Utils.TimeSpans/TimeSpansUtil.cs
+++ src/Soenneker.Utils.TimeSpans/TimeSpansUtil.cs
@@ -3,6 +3,9 @@ using System.Diagnostics.Contracts;
 
 namespace Soenneker.Utils.TimeSpans;
 
+/// <summary>
+/// Represents the time spans util.
+/// </summary>
 public static class TimeSpansUtil
 {
     /// <returns>Very first instant of day (smallest possible TimeSpan)</returns>
